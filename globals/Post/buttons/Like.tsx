16f1cc9601f5b0f5acac22f49d34e39fe3681a49
root <milosz20031207@gmail.com>
2023-06-16 16:20:13 +0000

    feature: added optimistic updates to liked posts

--- globals/Post/buttons/Like.tsx
+++ globals/Post/buttons/Like.tsx
@@ -1,7 +1,7 @@
 import { FC, useEffect, useState } from 'react'
 import Icon from '@globals/Icon'
 import { useLikePostMutation, useUserLikedPostQuery } from '../../../graphql/codegen/gql/gql'
-import { client } from '../../../router/graphqlClient'
+import { client, queryClient } from '../../../router/graphqlClient'
 
 interface LikeProps {
     id: string
@@ -13,10 +13,41 @@ const Like: FC<LikeProps> = ( { id, liked } ) => {
     console.log( liked )
 
     const [ open, setOpen ] = useState<boolean>( () => liked )
-    const { isLoading, mutate } = useLikePostMutation( client )
-    
+    const { isLoading, mutate, data } = useLikePostMutation( client, {
+        onMutate: async ( newLike ) => {
+            // Cancel any outgoing refetches
+            // (so they don't overwrite our optimistic update)
+            await queryClient.cancelQueries({ queryKey: ['LikePost'] })
+        
+            // Snapshot the previous value
+            const previousTodos = queryClient.getQueryData(['LikePost'])
+        
+            // Optimistically update to the new value
+            queryClient.setQueryData(['LikePost'], (old) => newLike )
+        
+            setOpen( !!newLike.like )
+
+            // Return a context object with the snapshotted value
+            return { previousTodos }
+          },
+          // If the mutation fails,
+          // use the context returned from onMutate to roll back
+          onError: (err, _, context) => {
+            queryClient.setQueryData(['LikePost'], (context as any).previousTodos)
+            setOpen( !!(context as any).previousTodos )
+          },
+          // Always refetch after error or success:
+          onSettled: () => {
+            queryClient.invalidateQueries({ queryKey: ['LikePost'] })
+          },
+    } )
+
+    // useEffect( () => {
+    //     setOpen( !!data?.likePost?.like )
+    // }, [ data ] )
+
     const handleLike: () => void = () => {
-        setOpen( !open )
+        // setOpen( !open )
         mutate( {
             postId: id,
             like: !open
