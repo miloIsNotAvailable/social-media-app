127db85b659dd004303f3200cb3cea6afcccac6c
root <milosz20031207@gmail.com>
2023-06-21 16:50:55 +0000

    layout: added layout for displaying specific community

---
+++ pages/index/communities.[id].tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Community from "../../components/community";
+// import {} from '../../'
+
+const Community_: FC = () => {
+
+    return (
+        <Community/>
+    )
+}
+
+export default Community_
