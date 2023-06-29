52917f922d85e61294240bec304830c90a999dff
root <milosz20031207@gmail.com>
2023-06-29 16:53:59 +0000

    layout: added displaying queried community data

--- components/community/layouts/CommunityHeader.tsx
+++ components/community/layouts/CommunityHeader.tsx
@@ -2,21 +2,35 @@ import { FC } from "react";
 import Profile from "@globals/Profile";
 import NavRoute from "@globals/NavRoute";
 import { styles } from '../styles'
+import { CommunityDetails, useQueryCommunityQuery } from "../../../graphql/codegen/gql/gql";
+import { client } from "../../../router/graphqlClient";
+import { useLocation, useParams } from "react-router-dom";
 
 const CommunityHeader: FC = () => {
+
+    const { id } = useParams() as { id: string }
+
+    const { data, isLoading } = useQueryCommunityQuery( client, { 
+        communityId: id,
+        includePosts: false 
+    } )
+
+    const { pathname } =  useLocation()
 
     return (
         <div className={ styles.community_header }>
             <div className={ styles.community_name }>
                 <Profile/>
                 <NavRoute 
-                    mainpage={ "home" } 
-                    section={ "communities" }
-                    element={ "lorem-ipsum" }
+                    // mainpage={ "home" } 
+                    // section={ "communities" }
+                    // element={ "lorem-ipsum" }
+                    link={ isLoading ? "hey" : ("/communities/" + (data?.queryCommunity as CommunityDetails).title) }
+                    to={ pathname }
                 />
             </div>
             <div className={ styles.community_desc }>
-                this is a community description 
+                { isLoading ? "loading..." : (data?.queryCommunity as CommunityDetails).description } 
             </div>
         </div>
     )
