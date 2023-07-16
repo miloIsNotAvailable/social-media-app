09be25712ba1ec3e2256def8e90e70071fe6682c
root <milosz20031207@gmail.com>
2023-07-16 16:10:34 +0000

    layout: added search icon

--- components/Navbar/menu/MenuIcon.tsx
+++ components/Navbar/menu/MenuIcon.tsx
@@ -4,22 +4,24 @@ import Icon from "../../custom/Icon";
 const MenuIcon: FC = () => {
 
     return (
-        <Icon>
-            <svg
-                width="18"
-                height="7"
-                viewBox="0 0 18 7"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-            <g id="Group 36">
-                <g id="Group 35">
-                <line id="Line 26" y1="0.5" x2="18" y2="0.5" stroke="white" />
-                <line id="Line 27" y1="6.5" x2="18" y2="6.5" stroke="white" />
+        <>
+            <Icon>
+                <svg
+                    width="18"
+                    height="7"
+                    viewBox="0 0 18 7"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                <g id="Group 36">
+                    <g id="Group 35">
+                    <line id="Line 26" y1="0.5" x2="18" y2="0.5" stroke="white" />
+                    <line id="Line 27" y1="6.5" x2="18" y2="6.5" stroke="white" />
+                    </g>
                 </g>
-            </g>
-            </svg>
-        </Icon>
+                </svg>
+            </Icon>
+        </>
     );
 }
 
--- components/home/navbars/TopNavbar.tsx
+++ components/home/navbars/TopNavbar.tsx
@@ -39,9 +39,19 @@ const TopNavbar: FC = () => {
             </Suspense>
 
             <NavRoute mainpage={ loc_arr[1] } section={ loc_arr[2] } element={ loc_arr[3] }/>
-            <Suspense fallback={ <Loading width={ "1rem" } height={ "1rem" }/> }>
-                <Profile/>
-            </Suspense>
+            <div className={ styles.search_profile_wrap }>
+                <Suspense fallback={ <Loading width={ "1rem" } height={ "1rem" }/> }>
+                    <Icon>
+                        <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="7.5" cy="8" r="7" stroke="white"/>
+                            <line x1="17.6464" y1="17.8536" x2="12.6464" y2="12.8536" stroke="white"/>
+                        </svg>
+                    </Icon>
+                </Suspense>
+                <Suspense fallback={ <Loading width={ "1rem" } height={ "1rem" }/> }>
+                    <Profile/>
+                </Suspense>
+            </div>
         </nav>
     )
 }
