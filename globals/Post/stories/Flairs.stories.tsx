d946381b0f9a009e84b01666b5ae8e0d563943f0
root <milosz20031207@gmail.com>
2023-08-01 21:48:59 +0000

    fix: fixed flair layout padding and width

---
+++ globals/Post/stories/Flairs.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Flair from '../scenes/Flair';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../../../router/graphqlClient';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'globals/Post/Flairs',
+  component: Flair,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+} as ComponentMeta<typeof Flair>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Flair> = (args) => 
+<QueryClientProvider client={ queryClient }>
+<BrowserRouter>
+<Flair {...args} /></BrowserRouter>
+</QueryClientProvider>
+
+export const Primary = Template.bind({});
+Primary.args = {
+    content: "hello"
+}
